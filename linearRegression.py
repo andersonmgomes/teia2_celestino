7029be1b54b62a1812da0fc10306edb52128bdbf
Anderson Martins <andersonmg@gmail.com>
2021-09-30 06:46:46 -0300

    new class

--- linearRegression.py
+++ linearRegression.py
@@ -1,4 +1,3 @@
-from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score, mean_squared_error
