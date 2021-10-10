050c4010e8308c849eb842a9e8804d1dfcc5dc14
Anderson Martins <andersonmg@gmail.com>
2021-10-10 18:59:22 -0300

    getBestConfusionMatrix()

--- 20211008-Trabalho03/trab03_utils.py
+++ 20211008-Trabalho03/trab03_utils.py
@@ -26,21 +26,3 @@ def getCorrHeatMap(ds, annot=True):
     return sns.heatmap(ds.corr(), cmap='coolwarm', fmt='.2f', linewidths=0.1,vmax=1.0, square=True
                        , linecolor='white', robust=True, annot=annot);
     
-import numpy as np
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-
-def getConfusionMatrixHeatMap(y_true_label, y_predict_label):
-    cf_matrix = confusion_matrix(y_true_label, y_predict_label)
-    group_names = ['True Neg','False Pos','False Neg','True Pos']
-    group_counts = ['{0:0.0f}'.format(value) for value in
-                    cf_matrix.flatten()]
-    group_percentages = ['{0:.2%}'.format(value) for value in
-                        cf_matrix.flatten()/np.sum(cf_matrix)]
-    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in
-            zip(group_names,group_counts,group_percentages)]
-    labels = np.asarray(labels).reshape(2,2)
-    return sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues');    
-
-
-
