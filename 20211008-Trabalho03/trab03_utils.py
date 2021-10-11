76ad97d8fec4110a2f6cfb63460224ad260524a2
Anderson Martins <andersonmg@gmail.com>
2021-10-11 09:35:00 -0300

    experments

--- 20211008-Trabalho03/trab03_utils.py
+++ 20211008-Trabalho03/trab03_utils.py
@@ -33,6 +33,16 @@ def getDSPriceHousing_ClassProb():
 def getDSWine_RED():
     return pd.read_csv(DS_PATH + 'winequality-red.csv', sep=';')
 
+def getDSWine_RED_ClassProb():
+    df = getDSWine_RED()
+    threashold = df.quality.describe()['75%']
+    df['high_quality'] = 0
+    for i, row in df.iterrows():
+        if row.quality > threashold:
+            df.loc[i, 'high_quality'] = 1
+    
+    return df.drop('quality', axis=1)
+
 def getCorrHeatMap(ds, annot=True):
     return sns.heatmap(ds.corr(), cmap='coolwarm', fmt='.2f', linewidths=0.1,vmax=1.0, square=True
                        , linecolor='white', robust=True, annot=annot);
